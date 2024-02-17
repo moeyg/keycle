354ff8b1b52614dde509d00b170faa2ce80aea82
moeyg <moooeyg@gmail.com>
2024-02-17 21:48:55 +0900

    feat: submitUserAnswers() 메서드 추가
    이 커밋은 submitUserAnswers() 메서드를 추가했습니다. 이 메서드는 사용자의 답안을 서버로 전송하고, 성공적으로 전송되면 사용자의 답안을 로컬 스토리지에서 지우고 홈 페이지로 리다이렉트하는 기능을 수행합니다.

--- assets/scripts/components/result.js
+++ assets/scripts/components/result.js
@@ -1,5 +1,6 @@
 export default class Result {
   constructor() {
+    this.homeButton = document.querySelector('#home-button');
     this.userAnswers = JSON.parse(localStorage.getItem('userAnswers'));
     this.quizCard = document.querySelector('.quiz-card');
     this.qrcode = document.querySelector('.qrcode');
@@ -22,8 +23,43 @@ export default class Result {
     }
   }
 
+  submitUserAnswers() {
+    this.homeButton.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const storedAnswers = localStorage.getItem('userAnswers');
+      const userAnswers = storedAnswers ? JSON.parse(storedAnswers) : [];
+
+      fetch('3.37.238.149/stats/correctRateUpdate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userAnswers }),
+      })
+        .then((response) => {
+          console.log(response);
+
+          if (!response.ok) {
+            throw new Error('사용자 답안 전송 실패');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log('사용자 답안 전송 성공:', data);
+        })
+        .catch((error) => {
+          console.error('사용자 답안 전송 에러:', error);
+        });
+
+      localStorage.clear();
+      window.location.href = '../../index.html';
+    });
+  }
+
   run() {
     this.calculateCorrectCount();
     this.renderScore();
+    this.submitUserAnswers();
   }
 }
