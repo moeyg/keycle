dd5a3d6e9ab42c9e89cd85c25b942e3e1c6263be
Gaeon <gaeon0413@gmail.com>
2024-02-17 15:16:03 +0900

    feat: DB 모델 작성

--- KEYCLE/stats/models.py
+++ KEYCLE/stats/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Stats(models.Model):
+	questionId = models.AutoField(primary_key = True)
+	correctAnswer = models.IntegerField()
+
+	class Meta:
+		# 모델의 내용이 변경 가능하면 Ture, 변경 불가능하면 False 
+		managed = False
+		db_table = 'Stats'
