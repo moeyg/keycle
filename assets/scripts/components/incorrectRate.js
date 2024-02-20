52e3bc6a06fa4ec52cfe3881613a7a099d1194a5
moeyg <moooeyg@gmail.com>
2024-02-20 20:08:12 +0900

    refactor: constructor 참조 오류 수정
    이 커밋은 클래스 내부에서 constructor를 제대로 참조하지 않는 오류를 수정하였습니다. 이로써 코드의 가독성과 유지보수성이 향상되었습니다.

--- assets/scripts/components/incorrectRate.js
+++ assets/scripts/components/incorrectRate.js
@@ -2,13 +2,13 @@ export default class IncorrectRate {
   constructor() {
     this.commentaryURL = window.location.pathname;
     this.regex = /quiz-(\d+)/;
-    this.index = parseInt(commentaryURL.match(regex)[1]) - 1;
+    this.index = parseInt(this.commentaryURL.match(this.regex)[1]) - 1;
     this.rateValue = document.querySelector('#rate-value');
   }
 
   setIncorrectRate() {
     const incorrectRate = JSON.parse(localStorage.getItem('incorrectRate'));
-    const value = incorrectRate[index];
+    const value = incorrectRate[this.index];
     this.rateValue.innerText = `${value}%`;
   }
 }
